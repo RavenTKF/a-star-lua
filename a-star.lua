3b099f0101e20d02f810611813f0ae03c9ae1bae
Mingfei <raventkf@gmail.com>
2019-04-13 11:13:42 +0800

    Update a-star.lua

--- a-star.lua
+++ a-star.lua
@@ -1,5 +1,6 @@
 -- ======================================================================
 -- Copyright (c) 2012 RapidFire Studio Limited 
+-- Modified By RavenTKF
 -- All Rights Reserved. 
 -- http://www.rapidfirestudio.com
 
@@ -22,43 +23,54 @@
 -- TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 -- SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 -- ======================================================================
+astar = astar or BaseClass()
 
-module ( "astar", package.seeall )
+function astar:__init()
+    astar.Instance = self
 
-----------------------------------------------------------------
--- local variables
-----------------------------------------------------------------
+	self.INF = 1/0
+	self.cachedPaths = nil
+end
 
-local INF = 1/0
-local cachedPaths = nil
-
+function astar:getInstance()
+	if astar.Instance == nil then
+		astar.New()
+	end
+	return astar.Instance
+end
 ----------------------------------------------------------------
 -- local functions
 ----------------------------------------------------------------
 
-function dist ( x1, y1, x2, y2 )
+function astar:dist ( x1, y1, x2, y2 )
 	
 	return math.sqrt ( math.pow ( x2 - x1, 2 ) + math.pow ( y2 - y1, 2 ) )
 end
 
-function dist_between ( nodeA, nodeB )
+function astar:dist_between ( nodeA, nodeB )
 
-	return dist ( nodeA.x, nodeA.y, nodeB.x, nodeB.y )
+	return self:dist ( nodeA.x, nodeA.y, nodeB.x, nodeB.y )
 end
 
-function heuristic_cost_estimate ( nodeA, nodeB )
+function astar:heuristic_cost_estimate ( nodeA, nodeB )
 
-	return dist ( nodeA.x, nodeA.y, nodeB.x, nodeB.y )
+	return self:dist ( nodeA.x, nodeA.y, nodeB.x, nodeB.y )
 end
 
-function is_valid_node ( node, neighbor )
+function astar:is_valid_node ( node, neighbor )
 
-	return true
+	local MAX_DIST = 1.2
+		
+	if 	neighbor.player_id == node.player_id and 
+		self:dist ( node.x, node.y, neighbor.x, neighbor.y ) < MAX_DIST then
+		return true
+	end
+	return false
 end
 
-function lowest_f_score ( set, f_score )
+function astar:lowest_f_score ( set, f_score )
 
-	local lowest, bestNode = INF, nil
+	local lowest, bestNode = self.INF, nil
 	for _, node in ipairs ( set ) do
 		local score = f_score [ node ]
 		if score < lowest then
@@ -68,18 +80,18 @@ function lowest_f_score ( set, f_score )
 	return bestNode
 end
 
-function neighbor_nodes ( theNode, nodes )
+function astar:neighbor_nodes ( theNode, nodes )
 
 	local neighbors = {}
 	for _, node in ipairs ( nodes ) do
-		if theNode ~= node and is_valid_node ( theNode, node ) then
+		if theNode ~= node and self:is_valid_node ( theNode, node ) then
 			table.insert ( neighbors, node )
 		end
 	end
 	return neighbors
 end
 
-function not_in ( set, theNode )
+function astar:not_in ( set, theNode )
 
 	for _, node in ipairs ( set ) do
 		if node == theNode then return false end
@@ -87,7 +99,7 @@ function not_in ( set, theNode )
 	return true
 end
 
-function remove_node ( set, theNode )
+function astar:remove_node ( set, theNode )
 
 	for i, node in ipairs ( set ) do
 		if node == theNode then 
@@ -98,11 +110,11 @@ function remove_node ( set, theNode )
 	end	
 end
 
-function unwind_path ( flat_path, map, current_node )
+function astar:unwind_path ( flat_path, map, current_node )
 
 	if map [ current_node ] then
 		table.insert ( flat_path, 1, map [ current_node ] ) 
-		return unwind_path ( flat_path, map, map [ current_node ] )
+		return self:unwind_path ( flat_path, map, map [ current_node ] )
 	else
 		return flat_path
 	end
@@ -112,41 +124,39 @@ end
 -- pathfinding functions
 ----------------------------------------------------------------
 
-function a_star ( start, goal, nodes, valid_node_func )
+function astar:a_star ( start, goal, nodes )
 
 	local closedset = {}
 	local openset = { start }
 	local came_from = {}
 
-	if valid_node_func then is_valid_node = valid_node_func end
-
 	local g_score, f_score = {}, {}
 	g_score [ start ] = 0
-	f_score [ start ] = g_score [ start ] + heuristic_cost_estimate ( start, goal )
+	f_score [ start ] = g_score [ start ] + self:heuristic_cost_estimate ( start, goal )
 
 	while #openset > 0 do
 	
-		local current = lowest_f_score ( openset, f_score )
+		local current = self:lowest_f_score ( openset, f_score )
 		if current == goal then
-			local path = unwind_path ( {}, came_from, goal )
+			local path = self:unwind_path ({}, came_from, goal )
 			table.insert ( path, goal )
 			return path
 		end
 
-		remove_node ( openset, current )		
+		self:remove_node ( openset, current )		
 		table.insert ( closedset, current )
 		
-		local neighbors = neighbor_nodes ( current, nodes )
+		local neighbors = self:neighbor_nodes ( current, nodes )
 		for _, neighbor in ipairs ( neighbors ) do 
-			if not_in ( closedset, neighbor ) then
+			if self:not_in ( closedset, neighbor ) then
 			
-				local tentative_g_score = g_score [ current ] + dist_between ( current, neighbor )
+				local tentative_g_score = g_score [ current ] + self:dist_between ( current, neighbor )
 				 
-				if not_in ( openset, neighbor ) or tentative_g_score < g_score [ neighbor ] then 
+				if self:not_in ( openset, neighbor ) or tentative_g_score < g_score [ neighbor ] then 
 					came_from 	[ neighbor ] = current
 					g_score 	[ neighbor ] = tentative_g_score
-					f_score 	[ neighbor ] = g_score [ neighbor ] + heuristic_cost_estimate ( neighbor, goal )
-					if not_in ( openset, neighbor ) then
+					f_score 	[ neighbor ] = g_score [ neighbor ] + self:heuristic_cost_estimate ( neighbor, goal )
+					if self:not_in ( openset, neighbor ) then
 						table.insert ( openset, neighbor )
 					end
 				end
@@ -160,29 +170,37 @@ end
 -- exposed functions
 ----------------------------------------------------------------
 
-function clear_cached_paths ()
+function astar:clear_cached_paths ()
 
-	cachedPaths = nil
+	self.cachedPaths = nil
 end
 
-function distance ( x1, y1, x2, y2 )
-	
-	return dist ( x1, y1, x2, y2 )
-end
+function astar:path ( start, goal, nodes, ignore_cache )
 
-function path ( start, goal, nodes, ignore_cache, valid_node_func )
-
-	if not cachedPaths then cachedPaths = {} end
-	if not cachedPaths [ start ] then
-		cachedPaths [ start ] = {}
-	elseif cachedPaths [ start ] [ goal ] and not ignore_cache then
-		return cachedPaths [ start ] [ goal ]
+	if not self.cachedPaths then self.cachedPaths = {} end
+	if not self.cachedPaths [ start ] then
+		self.cachedPaths [ start ] = {}
+	elseif self.cachedPaths [ start ] [ goal ] and not ignore_cache then
+		return self.cachedPaths [ start ] [ goal ]
 	end
 
-      local resPath = a_star ( start, goal, nodes, valid_node_func )
-      if not cachedPaths [ start ] [ goal ] and not ignore_cache then
-              cachedPaths [ start ] [ goal ] = resPath
+      local resPath = self:a_star ( start, goal, nodes )
+      if not self.cachedPaths [ start ] [ goal ] and not ignore_cache then
+              self.cachedPaths [ start ] [ goal ] = resPath
       end
 
 	return resPath
+end
+
+function astar:getNodeID ( x, y )          --using in 20x20 map (x:[-10,10] y:[-10,10])
+	local x_n = math.ceil((x+10)/1)    
+	if x_n == 0 then
+		x_n = x_n +1
+	end
+
+	local y_n = math.ceil(math.abs(y-10)/1)
+	if y_n == 0 then
+		y_n = y_n +1
+	end
+	return  x_n + (y_n - 1) * 20 
 end
