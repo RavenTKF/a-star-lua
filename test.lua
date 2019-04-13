700d5d359b4b6b4be0d866aadcb2d050b5322430
Mingfei <raventkf@gmail.com>
2019-04-13 11:52:27 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,5 +1,6 @@
 -- ======================================================================
 -- Copyright (c) 2012 RapidFire Studio Limited 
+-- Modified By RavenTKF
 -- All Rights Reserved. 
 -- http://www.rapidfirestudio.com
 
@@ -22,52 +23,17 @@
 -- TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 -- SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 -- ======================================================================
+require "astar"
+require "maphandler"
 
-require "a-star"
+local maphandler = Maphandler.New()
+local astar = astar.New()
 
-local graph = {}
-graph [ 1 ] = {}
-graph [ 1 ].id = 1
-graph [ 1 ].x = 0
-graph [ 1 ].y = 0
-graph [ 1 ].player_id = 1
+local graph1 = maphandler:GetMap1()
+local graph2 = maphandler:GetMap2()
+local graph3 = maphandler:GetMap3()
 
-graph [ 2 ] = {}
-graph [ 2 ].id = 2
-graph [ 2 ].x = 200
-graph [ 2 ].y = 200
-graph [ 2 ].player_id = 1
-
-graph [ 3 ] = {}
-graph [ 3 ].id = 3
-graph [ 3 ].x = -200
-graph [ 3 ].y = 200
-graph [ 3 ].player_id = 1
-
-graph [ 4 ] = {}
-graph [ 4 ].id = 4
-graph [ 4 ].x = 200
-graph [ 4 ].y = -200
-graph [ 4 ].player_id = 2
-
-graph [ 5 ] = {}
-graph [ 5 ].id = 5
-graph [ 5 ].x = -200
-graph [ 5 ].y = -200
-graph [ 5 ].player_id = 2
-
-local valid_node_func = function ( node, neighbor ) 
-	
-	local MAX_DIST = 300
-		
-	if 	neighbor.player_id == node.player_id and 
-		astar.distance ( node.x, node.y, neighbor.x, neighbor.y ) < MAX_DIST then
-		return true
-	end
-	return false
-end
-
-local path = astar.path ( graph [ 2 ], graph [ 3 ], graph, true, valid_node_func )
+local path = astar:path ( graph3 [ astar:getNodeID(-2.21,9.46) ], graph3 [ astar:getNodeID(6.49,-8.51) ], graph3, true, valid_node_func )
 
 if not path then
 	print ( "No valid path found" )
